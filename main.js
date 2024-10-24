965c6980f08c466146270101819b146ed944e4c9
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2024-10-24 18:43:04 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const container = document.getElementById("container");
+
+// How I can make a flexbox of grid divs in precision by 16x16?
+//
+
+const dFrag = document.createDocumentFragment();
+let default_grid_size = 16;
+let default_grid = default_grid_size;
+
+for (let i = 0; i < default_grid * default_grid; i++) {
+
+	const div_grid = document.createElement("div");
+	div_grid.setAttribute("class", "div_grid");
+	if (i % 16 == 0) {
+		div_grid.style.display = "block";
+		dFrag.appendChild(div_grid);
+	}
+	else {
+		div_grid.style.display = "inline";
+		dFrag.appendChild(div_grid);
+	}
+}
+
+container.appendChild(dFrag);
